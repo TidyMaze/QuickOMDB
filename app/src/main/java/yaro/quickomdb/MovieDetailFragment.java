0af54d5a92641042749cd59a4ca19c3176634065
TidyMaze <GrosKiff@gmail.com>
2017-02-21 22:15:39 +0100

    Initial commit

---
+++ app/src/main/java/yaro/quickomdb/MovieDetailFragment.java
@@ -0,0 +1,59 @@
+package yaro.quickomdb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import yaro.quickomdb.dummy.DummyContent;
+import yaro.quickomdb.model.Movie;
+
+public class MovieDetailFragment extends Fragment {
+
+    public static final String ARG_ITEM_ID = "item_id";
+    private Movie mItem;
+
+    public MovieDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            Toolbar appBarLayout = (Toolbar) activity.findViewById(R.id.toolbar);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.getTitle());
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.movie_detail, container, false);
+
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.titleTextView)).setText(mItem.getTitle());
+            ((TextView) rootView.findViewById(R.id.plotTextView)).setText("not done yet");
+
+            Glide.with(rootView.getContext())
+                .load(mItem.getPoster())
+                .centerCrop()
+                .crossFade()
+                .into(((ImageView) rootView.findViewById(R.id.posterImageView)));
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/yaro/quickomdb/MovieListActivity.java
@@ -0,0 +1,137 @@
+package yaro.quickomdb;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import yaro.quickomdb.dummy.DummyContent;
+import yaro.quickomdb.model.Movie;
+
+import java.util.List;
+
+/**
+ * An activity representing a list of Movies. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link MovieDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class MovieListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        View recyclerView = findViewById(R.id.movie_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.movie_detail_container) != null) {
+            mTwoPane = true;
+        }
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<Movie> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<Movie> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mTitleView.setText(holder.mItem.getTitle());
+
+
+            Glide.with(MovieListActivity.this)
+                .load(holder.mItem.getPoster())
+                .centerCrop()
+                .crossFade()
+                .into(holder.mPosterView);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.getImdbId());
+                        MovieDetailFragment fragment = new MovieDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.movie_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, MovieDetailActivity.class);
+                        intent.putExtra(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.getImdbId());
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final ImageView mPosterView;
+            public final TextView mTitleView;
+            public Movie mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mPosterView = (ImageView) view.findViewById(R.id.posterImageView);
+                mTitleView = (TextView) view.findViewById(R.id.titleTextView);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mTitleView.getText() + "'";
+            }
+        }
+    }
+}
