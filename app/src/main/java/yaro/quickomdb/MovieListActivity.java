d1be5790d5db23093fce7c79f2457144abd851b9
TidyMaze <GrosKiff@gmail.com>
2017-02-22 22:02:46 +0100

    Clean up

--- app/src/main/java/yaro/quickomdb/MovieListActivity.java
+++ app/src/main/java/yaro/quickomdb/MovieListActivity.java
@@ -29,20 +29,8 @@ import yaro.quickomdb.service.OmdbService;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * An activity representing a list of Movies. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link MovieDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class MovieListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
     private boolean mTwoPane;
 
     @Override
